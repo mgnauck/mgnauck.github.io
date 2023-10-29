6e2fad7b06420a4a9741060f32a33714f22a37df
Markus Gnauck <17846097+mgnauck@users.noreply.github.com>
2023-10-29 20:46:50 +0100

    Directory restructuring

--- preview/scripts/demo.js
+++ preview/scripts/demo.js
@@ -127,17 +127,6 @@ function exitFullscreen()
   fullscreen = false;
 }
 
-function getRandomInt(max)
-{
-  return Math.floor(Math.random() * max);
-}
-
-function degToRad(deg)
-{
-  //return deg * Math.PI / 180.0;
-  return deg * 3.14159 / 180.0;
-}
-
 function fillBuffer(buffer, color)
 {
   const length = width * height;
---
+++ preview/scripts/utils.js
@@ -0,0 +1,10 @@
+function getRandomInt(max)
+{
+  return Math.floor(Math.random() * max);
+}
+
+function degToRad(deg)
+{
+  //return deg * Math.PI / 180.0;
+  return deg * 3.14159 / 180.0;
+}
